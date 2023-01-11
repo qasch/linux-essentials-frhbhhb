b893902b8a85437d4cfa7956c4c3d5e39e5af438
Kai Schell <kai.schell@signet.de>
2023-01-11 10:02:14 +0100

    scripts

---
+++ scripts/firstscript.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Shebang
+
+name="Gretl"
+
+echo Ich bin Zeile 1
+echo Ich bin Zeile 2
+echo Ich bin Zeile 3
+# Anzeige Inhalt Verzeichnis
+ls -lh
+echo Der Hund heisst $name
+echo letzte Zeile im Script
